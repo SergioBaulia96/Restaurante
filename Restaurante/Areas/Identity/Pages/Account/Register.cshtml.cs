8ffcaf2154e4a7e1c7cee7ea6fce9dc1f03eb3a2
SergioBaulia96 <sergiobrk2008@gmail.com>
2024-09-30 12:25:11 -0300

    LOGIN Y REGISTER

--- Restaurante/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Restaurante/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,8 +74,8 @@ namespace Restaurante.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required (ErrorMessage = "El campo de email es obligatorio.")]
+            [EmailAddress (ErrorMessage = "Por favor, introduce un email válido.")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -83,7 +83,7 @@ namespace Restaurante.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required (ErrorMessage = "La contraseña es obligatoria.")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -95,7 +95,7 @@ namespace Restaurante.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "La contraseña no coincide.")]
             public string ConfirmPassword { get; set; }
         }
 
